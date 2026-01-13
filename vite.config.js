ac690381f2bf2d76b6058430d9fc82a0daf75e54
Preston Schnell <64565042+prestonschnell@users.noreply.github.com>
2026-01-13 15:25:57 -0600

    changed path of deployment.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://github.com/prestonschnell/Lifting-Honduras/',
   plugins: [react()],
+  base: '/'
 })
